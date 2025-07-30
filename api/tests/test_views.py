f860f4104a83cc20e05a1114a2dc3c2a26aa984b
Javier Ocampo <ferroxido@gmail.com>
2025-07-30 13:29:55 +0100

    feat: add the missing methods, UPDATE, DELETE

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -39,6 +39,56 @@ class BookViewTest(APITestCase):
         new_book = Book.objects.get(isbn="9876543210987")
         self.assertEqual(new_book.title, "New Book")
 
+class BookDetailViewTest(APITestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Demo",
+            author="Author",
+            isbn="1234567890123",
+            published_date=date.today()
+        )
+
+    def test_get_single_book(self):
+        url = reverse('api:book_detail', kwargs={'id': self.book.id})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        body = response.json()
+        self.assertEqual(body["title"], self.book.title)
+
+    def test_get_single_book_not_found(self):
+        url = reverse('api:book_detail', kwargs={'id': 999})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_book(self):
+        url = reverse('api:book_detail', kwargs={'id': self.book.id})
+        data = {
+            "title": "Updated Title"
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Title")
+
+    def test_update_book_not_found(self):
+        url = reverse('api:book_detail', kwargs={'id': 999})
+        data = {
+            "title": "Updated Title"
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_book(self):
+        url = reverse('api:book_detail', kwargs={'id': self.book.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_delete_book_not_found(self):
+        url = reverse('api:book_detail', kwargs={'id': 999})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
 
 class HealthViewTest(APITestCase):
 
--- api/views.py
+++ api/views.py
@@ -34,3 +34,32 @@ class BookView(APIView):
 
 
 book_view = BookView.as_view()
+
+class BookDetailView(APIView):
+    def get(self, request, id, *args, **kwargs):
+        try:
+            book = Book.objects.get(id=id)
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        except Book.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, id, *args, **kwargs):
+        try:
+            book = Book.objects.get(id=id)
+            serializer = BookSerializer(book, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        except Book.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, id, *args, **kwargs):
+        try:
+            book = Book.objects.get(id=id)
+            book.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Book.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+book_detail_view = BookDetailView.as_view()
